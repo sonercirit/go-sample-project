c9a860d7ff38dce609ed31a9bc2499795f58aca8
sonercirit <sonercirit@gmail.com>
2020-03-06 00:22:48 +0300

    adds cars endpoint

--- car_pooling/init.go
+++ car_pooling/init.go
@@ -1,6 +1,7 @@
 package car_pooling
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -12,5 +13,18 @@ func Init() {
 			return
 		}
 		_, _ = fmt.Fprint(writer, "OK")
+	})
+
+	http.HandleFunc("/cars", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != http.MethodPut {
+			writer.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		err := json.NewDecoder(request.Body).Decode(&cars)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
 	})
 }
